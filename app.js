6f848b746575f6946973be54288fbba094bfa7a2
Muhammadshoh Rajabov <rajabovmuhammadshoh009@gmail.com>
2022-12-23 14:11:15 +0500

    feat: finish core functionality

--- app.js
+++ app.js
@@ -7,29 +7,94 @@ const app = Vue.createApp({
 		return {
 			monsterHealth: 100,
 			playerHealth: 100,
+			currentRound: 0,
+			winner: null,
+			logMessages: [],
 		}
+	},
+	watch: {
+		playerHealth(value) {
+			if (value <= 0 && this.monsterHealth <= 0) {
+				this.winner = 'draw'
+			} else if (value <= 0) {
+				this.winner = 'monster'
+			}
+		},
+		monsterHealth(value) {
+			if (value <= 0 && this.playerHealth <= 0) {
+				this.winner = 'draw'
+			} else if (value <= 0) {
+				this.winner = 'player'
+			}
+		},
 	},
 	computed: {
 		monsterBarHealth() {
+			if (this.monsterHealth < 0) {
+				return { width: 0 + '%' }
+			}
 			return {
 				width: this.monsterHealth + '%',
 			}
 		},
 		playerBarHealth() {
+			if (this.playerHealth < 0) {
+				return { width: 0 + '%' }
+			}
 			return {
 				width: this.playerHealth + '%',
 			}
 		},
+		useSpecialAttack() {
+			return this.currentRound % 3 !== 0
+		},
 	},
 	methods: {
+		surrender() {
+			this.winner = 'monster'
+		},
 		attackMonster() {
+			this.currentRound++
 			const attackValue = getRandomValue(5, 12)
 			this.monsterHealth -= attackValue
+			this.logAction('player', 'attack', attackValue)
 			this.attackPlayer()
 		},
 		attackPlayer() {
 			const attackValue = getRandomValue(8, 15)
 			this.playerHealth -= attackValue
+			this.logAction('monster', 'attack', attackValue)
+		},
+		specialAttackMonster() {
+			this.currentRound++
+			const attackValue = getRandomValue(10, 25)
+			this.monsterHealth -= attackValue
+			this.logAction('player', 'attack', attackValue)
+			this.attackPlayer()
+		},
+		healPlayer() {
+			this.currentRound++
+			const healValue = getRandomValue(8, 15)
+			this.playerHealth += healValue
+			if (this.playerHealth + healValue > 100) {
+				this.playerHealth = 100
+			}
+			this.logAction('player', 'heal', healValue)
+			this.attackPlayer()
+		},
+		startGame() {
+			this.logMessages = []
+			this.playerHealth = 100
+			this.monsterHealth = 100
+			this.winner = null
+			this.currentRound = 0
+		},
+		logAction(who, what, value) {
+			this.logMessages.unshift({
+				actionWho: who,
+				actionWhat: what,
+				actionValue: value,
+			})
 		},
 	},
 })
